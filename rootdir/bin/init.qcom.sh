1e9ee60a5d08f68b6aa45b03ae14388fa60abed0
SirRGB <sirrgb@proton.me>
2024-06-08 12:05:00 +0200

    msm8937-common: add basic msm8953 support
    * ref https://github.com/Mi-Thorium/android_device_xiaomi_mithorium-common/commit/66c8d81f8a4ec531af556b39a5ca9396788ff1a2
    * import media profiles from https://github.com/Moto-experimental/android_device_motorola_msm8953-common/tree/275996c5a7acfe45c95acfe355cb2055ee8ed664/configs/media
    * still needs blobs split
    
    Change-Id: Ibec4ee0c41e6f1238fa2a09462c247675e686758
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -360,35 +360,6 @@ case "$target" in
         ;;
     "msm8953")
 	start_msm_irqbalance_8939
-        if [ -f /sys/devices/soc0/soc_id ]; then
-            soc_id=`cat /sys/devices/soc0/soc_id`
-        else
-            soc_id=`cat /sys/devices/system/soc/soc0/id`
-        fi
-
-        if [ -f /sys/devices/soc0/hw_platform ]; then
-             hw_platform=`cat /sys/devices/soc0/hw_platform`
-        else
-             hw_platform=`cat /sys/devices/system/soc/soc0/hw_platform`
-        fi
-        case "$soc_id" in
-             "293" | "304" | "338" | "351" | "349" | "350" )
-                  case "$hw_platform" in
-                       "Surf")
-                                    setprop qemu.hw.mainkeys 0
-                                    ;;
-                       "MTP")
-                                    setprop qemu.hw.mainkeys 0
-                                    ;;
-                       "RCM")
-                                    setprop qemu.hw.mainkeys 0
-                                    ;;
-                       "QRD")
-                                    setprop qemu.hw.mainkeys 0
-                                    ;;
-                  esac
-                  ;;
-       esac
         ;;
     "sdm710")
         if [ -f /sys/devices/soc0/soc_id ]; then
